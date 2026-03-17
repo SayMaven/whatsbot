74b3ddbb6a27bc49d5e3ee66e07df6b256b1790a
Hackertrap <54194385+Hackertrap@users.noreply.github.com>
2026-03-18 04:09:03 +0700

    update gemini models

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const qrcode = require('qrcode-terminal');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash' });
 
 const client = new Client({
     authStrategy: new LocalAuth()
